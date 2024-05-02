bef697ee6b73ff3267f39b8c56f78ddca1bafaf2
Joshua Robinson <joshua@codeadventure.uk>
2024-05-02 15:30:44 +0100

    move from laptop to PC tower

--- __tests__/cpCalc.test.js
+++ __tests__/cpCalc.test.js
@@ -1,7 +1,17 @@
-import { cpCalc } from "../utility/cpCalc.js";
+const { cpCalc } = require("../utility/cpCalc.js")
 
 describe("CP Calc Testing", () => {
     test("Should return 0 if given no arguments", () => {
         expect(cpCalc()).toBe(0);
+    })
+    test("Should return 1 if given a meal of unit weight 100g containing 10g of carbs and a portion of 100g" , () => {
+        const unitWeight = 100
+        const carbs = 10
+        const portion = 100
+        expect(cpCalc(unitWeight, carbs, portion)).toBe(1);
+        expect(cpCalc(21, 11.2, 126)).toBe(1)
+    })
+    test("Should round to the nearest whole carb portion", () => {
+
     })
 })
--- utility/cpCalc.js
+++ utility/cpCalc.js
@@ -1,5 +1,7 @@
-export function cpCalc (unitWeight = 100, carbs = 0, portion = 0){
-    return carbs / unitWeight * portion;
+function cpCalc (unitWeight = 100, carbs = 0, portion = 0){
+    const totalCarbs = carbs / unitWeight * portion;
+    const cp = totalCarbs / 10;
+    return cp;
 }
 
 module.exports = { cpCalc }
