8f043b6e1ecaf7986d6bb202ad2dc1537c31b267
Joshua Robinson <joshua@codeadventure.uk>
2024-05-07 10:14:12 +0100

    Refactored unit tests to accept form object instead of three separate arguments

--- utility/cpCalc.js
+++ utility/cpCalc.js
@@ -1,5 +1,6 @@
-export function cpCalc(unitWeight = 100, carbs = 0, portion = 0){
-    const totalCarbs = carbs / unitWeight * portion;
+export function cpCalc({unitWeight, carbWeight, portionWeight}){
+    console.log(unitWeight, carbWeight, portionWeight);
+    const totalCarbs = carbWeight / unitWeight * portionWeight;
     const cp = totalCarbs / 10;
     return convertToHalves(cp);
 }
