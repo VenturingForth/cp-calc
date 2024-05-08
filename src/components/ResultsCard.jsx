acc18b14c278f03815cbd28218c800be77a15eb9
Joshua Robinson <joshua@codeadventure.uk>
2024-05-08 10:51:18 +0100

    Added dynamic messaging to the ResultsCard component

--- src/components/ResultsCard.jsx
+++ src/components/ResultsCard.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 
 export default function ResultsCard({ cpResult, isError }){
+    const [resultMessage, setResultMessage] = useState(`This meal has ${cpResult} CP in it.`);
+    useEffect(() => {
+        if (isError){
+            setResultMessage(`Please check you have filled in the form correctly.`)
+        } else {
+            setResultMessage(`This meal has ${cpResult} CP in it.`);
+        }
+    }, [isError, cpResult])
+
     return (
         <Card className="card-results" >
             <Card.Body>
-                This meal has {cpResult} CP.
+                <h3>Result:</h3>
+                {resultMessage}
             </Card.Body>
         </Card>
     )
